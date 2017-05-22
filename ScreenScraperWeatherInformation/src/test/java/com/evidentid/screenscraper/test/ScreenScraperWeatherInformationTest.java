0d16e20121d7821104f8f544200a04594ae19769
Akash Mandole <akashmandole@gmail.com>
2017-05-22 11:35:02 -0700

    Updated Test cases and code styling
    Addressed following issues : 
    - Only one test case that basically tests if any output is returned, at least a known value of a specific location/time could have been asserted

--- ScreenScraperWeatherInformation/src/test/java/com/evidentid/screenscraper/test/ScreenScraperWeatherInformationTest.java
+++ ScreenScraperWeatherInformation/src/test/java/com/evidentid/screenscraper/test/ScreenScraperWeatherInformationTest.java
@@ -14,59 +14,83 @@ import junit.framework.TestSuite;
 /**
  * Unit test for ScreenScrapper
  */
-public class ScreenScraperWeatherInformationTest extends TestCase{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public ScreenScraperWeatherInformationTest( String testName )
-    {
-        super( testName );
-    }
+public class ScreenScraperWeatherInformationTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public ScreenScraperWeatherInformationTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ScreenScraperWeatherInformationTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(ScreenScraperWeatherInformationTest.class);
+	}
 
-    /*
-     * Check JSON object validity
-     */
-    public void testScreenScraper()
-    {
-    	ScreenScraperWeatherInformation screenScraperWeatherInformation = new ScreenScraperWeatherInformation();
-    	JSONObject obj = screenScraperWeatherInformation.screenScraper("Atlanta, Georgia", "5/17/2017");
-    	
-    	assertNotNull("JSON data successfully fetched");
-    	
-    	assertTrue(obj.containsKey("Mean Temperature Actual"));
-    	assertTrue(obj.containsKey("Mean Temperature Average"));
-    	
-    	assertTrue(obj.containsKey("Max Temperature Actual"));
-    	assertTrue(obj.containsKey("Max Temperature Average"));
-    	assertTrue(obj.containsKey("Max Temperature Record"));
-    	
-    	assertTrue(obj.containsKey("Min Temperature Actual"));
-    	assertTrue(obj.containsKey("Min Temperature Average"));
-    	assertTrue(obj.containsKey("Min Temperature Record"));
-    	
-    }
-    
-    /*
-     * Test Connection
-     */
-    public void testConnectionObject()
-    {
-    	try {
-    		ScreenScraperWeatherInformation screenScraperWeatherInformation = new ScreenScraperWeatherInformation();
-			assertEquals("Connection successfull",screenScraperWeatherInformation.getConnectionObject("Atlanta, Georgia", "17","5","2017").getResponseCode(),HttpURLConnection.HTTP_OK);
+	/*
+	 * Check JSON object validity
+	 */
+	public void testScreenScraper() {
+		ScreenScraperWeatherInformation screenScraperWeatherInformation = new ScreenScraperWeatherInformation();
+		JSONObject obj = screenScraperWeatherInformation.screenScraper(
+				"Atlanta, Georgia", "5/18/2017");
+
+		String meanTemperatureActual = "79";
+		String meanTemperatureAverage = "70";
+
+		String maxTemperatureActual = "89";
+		String maxTemperatureAverage = "81";
+		String maxTemperatureRecord = "90";
+
+		String minTemperatureActual = "68";
+		String minTemperatureAverage = "59";
+		String minTemperatureRecord = "44";
+
+		assertNotNull("JSON data successfully fetched");
+
+		if (obj != null) {
+
+			assertEquals(obj.get(ScreenScraperWeatherInformation
+					.getMeanTemperatureActual()), meanTemperatureActual);
+			assertEquals(obj.get(ScreenScraperWeatherInformation
+					.getMeanTemperatureAverage()), meanTemperatureAverage);
+
+			assertEquals(obj.get(ScreenScraperWeatherInformation
+					.getMaxTemperatureActual()), maxTemperatureActual);
+			assertEquals(obj.get(ScreenScraperWeatherInformation
+					.getMaxTemperatureAverage()), maxTemperatureAverage);
+			assertEquals(obj.get(ScreenScraperWeatherInformation
+					.getMaxTemperatureRecord()), maxTemperatureRecord);
+
+			assertEquals(obj.get(ScreenScraperWeatherInformation
+					.getMinTemperatureActual()), minTemperatureActual);
+			assertEquals(obj.get(ScreenScraperWeatherInformation
+					.getMinTemperatureAverage()), minTemperatureAverage);
+			assertEquals(obj.get(ScreenScraperWeatherInformation
+					.getMinTemperatureRecord()), minTemperatureRecord);
+
+		}
+
+	}
+
+	/*
+	 * Test Connection
+	 */
+	public void testConnectionObject() {
+		try {
+			ScreenScraperWeatherInformation screenScraperWeatherInformation = new ScreenScraperWeatherInformation();
+			assertEquals(
+					"Connection successfull",
+					screenScraperWeatherInformation.getConnectionObject(
+							"Atlanta, Georgia", "17", "5", "2017")
+							.getResponseCode(), HttpURLConnection.HTTP_OK);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    }
+	}
 }
-
