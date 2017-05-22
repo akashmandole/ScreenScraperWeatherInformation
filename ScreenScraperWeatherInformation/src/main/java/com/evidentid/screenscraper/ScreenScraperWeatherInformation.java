ab589c0335374029abc53fad05862635f115da76
Akash Mandole <akashmandole@gmail.com>
2017-05-22 11:33:38 -0700

    Updated bugs and code styling
    Addressed following issues :
    - Output does not get printed in main method
    - Use of inelegant string concatenation
    - Lack of access modifiers (everything is public)
    - Lack of using final, string constants
    - Some code styling differences within the code
    - Unsafe url encoding of post parameters

--- ScreenScraperWeatherInformation/src/main/java/com/evidentid/screenscraper/ScreenScraperWeatherInformation.java
+++ ScreenScraperWeatherInformation/src/main/java/com/evidentid/screenscraper/ScreenScraperWeatherInformation.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
@@ -18,10 +19,25 @@ import org.jsoup.select.Elements;
  */
 public class ScreenScraperWeatherInformation {
 
-	String baseURL = "https://www.wunderground.com";
+	private final static String baseURL = "https://www.wunderground.com";
+
+	private final static String tableName = "historyTable";
+	private final static String className = "wx-value";
+
+	private final static String Mean_Temperature_Actual = "Mean Temperature Actual";
+	private final static String Mean_Temperature_Average = "Mean Temperature Average";
+
+	private final static String Max_Temperature_Actual = "Max Temperature Actual";
+	private final static String Max_Temperature_Average = "Max Temperature Average";
+	private final static String Max_Temperature_Record = "Max Temperature Record";
+
+	private final static String Min_Temperature_Actual = "Min Temperature Actual";
+	private final static String Min_Temperature_Average = "Min Temperature Average";
+	private final static String Min_Temperature_Record = "Min Temperature Record";
 
 	/*
-	 * The function takes input as location and date and outputs a JSON representation of Temperature data.
+	 * The function takes input as location and date and outputs a JSON
+	 * representation of Temperature data.
 	 */
 	public JSONObject screenScraper(String location, String date) {
 
@@ -34,12 +50,13 @@ public class ScreenScraperWeatherInformation {
 		String year = parts[2];
 		try {
 
-			HttpURLConnection conn = getConnectionObject(location,day,month,year);
+			HttpURLConnection conn = getConnectionObject(location, day, month,
+					year);
 
 			StringBuffer html = getHTML(conn);
-		
+
 			jsonObject = parseHTML(html.toString());
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -50,16 +67,13 @@ public class ScreenScraperWeatherInformation {
 	 * The function parses HTML document and creates a json object
 	 */
 	@SuppressWarnings("unchecked")
-	public static JSONObject parseHTML(String html) {
+	private static JSONObject parseHTML(String html) {
 		JSONObject jsonObject = new JSONObject();
 		Document doc = Jsoup.parse(html.toString());
-		
-		//get title 
-		 System.out.println("title : " + doc.title());
 
 		// get all temperatures
-		Element table = doc.getElementById("historyTable");
-		Elements rows = table.getElementsByClass("wx-value");
+		Element table = doc.getElementById(tableName);
+		Elements rows = table.getElementsByClass(className);
 
 		Element meanTemperatureActual = rows.get(0);
 		Element meanTemperatureAverage = rows.get(1);
@@ -72,24 +86,16 @@ public class ScreenScraperWeatherInformation {
 		Element minTemperatureAverage = rows.get(6);
 		Element minTemperatureRecord = rows.get(7);
 
-		jsonObject.put("Mean Temperature Actual",
-				meanTemperatureActual.text());
-		jsonObject.put("Mean Temperature Average",
-				meanTemperatureAverage.text());
+		jsonObject.put(Mean_Temperature_Actual, meanTemperatureActual.text());
+		jsonObject.put(Mean_Temperature_Average, meanTemperatureAverage.text());
 
-		jsonObject.put("Max Temperature Actual",
-				maxTemperatureActual.text());
-		jsonObject.put("Max Temperature Average",
-				maxTemperatureAverage.text());
-		jsonObject.put("Max Temperature Record",
-				maxTemperatureRecord.text());
+		jsonObject.put(Max_Temperature_Actual, maxTemperatureActual.text());
+		jsonObject.put(Max_Temperature_Average, maxTemperatureAverage.text());
+		jsonObject.put(Max_Temperature_Record, maxTemperatureRecord.text());
 
-		jsonObject.put("Min Temperature Actual",
-				minTemperatureActual.text());
-		jsonObject.put("Min Temperature Average",
-				minTemperatureAverage.text());
-		jsonObject.put("Min Temperature Record",
-				minTemperatureRecord.text());
+		jsonObject.put(Min_Temperature_Actual, minTemperatureActual.text());
+		jsonObject.put(Min_Temperature_Average, minTemperatureAverage.text());
+		jsonObject.put(Min_Temperature_Record, minTemperatureRecord.text());
 
 		return jsonObject;
 	}
@@ -97,7 +103,8 @@ public class ScreenScraperWeatherInformation {
 	/*
 	 * The function reads the connection stream and returns a HTML document;
 	 */
-	public static StringBuffer getHTML(HttpURLConnection conn) throws IOException {
+	private static StringBuffer getHTML(HttpURLConnection conn)
+			throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				conn.getInputStream()));
 		String inputLine;
@@ -113,10 +120,13 @@ public class ScreenScraperWeatherInformation {
 	/*
 	 * The function creates HttpURLConnection object using location and date
 	 */
-	public HttpURLConnection getConnectionObject(String location,
-			String day, String month, String year) throws IOException {
-		
-		URL obj = new URL(baseURL + "/cgi-bin/findweather/getForecast");
+	public HttpURLConnection getConnectionObject(String location, String day,
+			String month, String year) throws IOException {
+
+		StringBuilder forecastUrl = new StringBuilder(baseURL);
+		forecastUrl.append("/cgi-bin/findweather/getForecast");
+
+		URL obj = new URL(forecastUrl.toString());
 		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
 		conn.setReadTimeout(5000);
 		conn.addRequestProperty("Accept",
@@ -133,20 +143,23 @@ public class ScreenScraperWeatherInformation {
 				"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0");
 
 		// Adding params
-		String urlParameters = "airportorwmo=query&backurl=/history/index.html&code="
-				+ location
-				+ "&day="
-				+ day
-				+ "&historytype=DailyHistory&month="
-				+ month
-				+ "&year="
-				+ year;
+		StringBuilder urlParameters = new StringBuilder(
+				"airportorwmo=query&backurl=/history/index.html");
+		urlParameters.append("&code=");
+		urlParameters.append(URLEncoder.encode(location, "UTF-8"));
+		urlParameters.append("&day=");
+		urlParameters.append(URLEncoder.encode(day, "UTF-8"));
+		urlParameters.append("&historytype=DailyHistory");
+		urlParameters.append("&month=");
+		urlParameters.append(URLEncoder.encode(month, "UTF-8"));
+		urlParameters.append("&year=");
+		urlParameters.append(URLEncoder.encode(year, "UTF-8"));
+
 		conn.setDoOutput(true);
 		DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-		wr.writeBytes(urlParameters);
+		wr.writeBytes(urlParameters.toString());
 		wr.flush();
 		wr.close();
-		System.out.println("Request URL ... " + baseURL);
 
 		boolean redirect = false;
 
@@ -159,43 +172,90 @@ public class ScreenScraperWeatherInformation {
 				redirect = true;
 		}
 
-		System.out.println("Response Code ... " + status);
-
 		if (redirect) {
 
 			// get redirect url from "location" header field
-			String newUrl = conn.getHeaderField("Location");
+			StringBuilder redirectUrl = new StringBuilder(baseURL);
+			redirectUrl.append(conn.getHeaderField("Location"));
 
 			// get the cookie if need, for login
 			String cookies = conn.getHeaderField("Set-Cookie");
 
 			// open the new connnection again
-			conn = (HttpURLConnection) new URL(baseURL + newUrl).openConnection();
+			conn = (HttpURLConnection) new URL(redirectUrl.toString())
+					.openConnection();
 			conn.setRequestProperty("Cookie", cookies);
 			conn.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
 			conn.addRequestProperty("User-Agent", "Mozilla");
 			conn.addRequestProperty("Referer", "google.com");
-
-			System.out.println("Redirect to URL : " + newUrl);
 
 		}
 		return conn;
 	}
 
 	/*
-	 * Input : argument 1 : location(example - Atlanta, Georgia), argument 2 : date mm/dd/yyyy (example - 5/17/2017) 
+	 * Input : argument 1 : location(example - Atlanta, Georgia), argument 2 :
+	 * date mm/dd/yyyy (example - 5/17/2017)
 	 */
-	public static void main(String[] args) {
-		
-		String location = "Atlanta, Georgia";
-		String date = "5/17/2017";
-		if(args.length!=0){
+	public static void main(String[] args) throws Exception {
+
+		String location = null, date = null;
+
+		if (args.length != 0) {
 			location = args[0];
 			date = args[1];
 		}
-	
+
+		if (location == null || date == null) {
+			throw new Exception("Input parameters missing");
+		}
+
 		ScreenScraperWeatherInformation screenScraperWeatherInformation = new ScreenScraperWeatherInformation();
 		screenScraperWeatherInformation.screenScraper(location, date);
+	}
+
+	public static String getBaseurl() {
+		return baseURL;
+	}
+
+	public static String getTablename() {
+		return tableName;
+	}
+
+	public static String getClassname() {
+		return className;
+	}
+
+	public static String getMeanTemperatureActual() {
+		return Mean_Temperature_Actual;
+	}
+
+	public static String getMeanTemperatureAverage() {
+		return Mean_Temperature_Average;
+	}
+
+	public static String getMaxTemperatureActual() {
+		return Max_Temperature_Actual;
+	}
+
+	public static String getMaxTemperatureAverage() {
+		return Max_Temperature_Average;
+	}
+
+	public static String getMaxTemperatureRecord() {
+		return Max_Temperature_Record;
+	}
+
+	public static String getMinTemperatureActual() {
+		return Min_Temperature_Actual;
+	}
+
+	public static String getMinTemperatureAverage() {
+		return Min_Temperature_Average;
+	}
+
+	public static String getMinTemperatureRecord() {
+		return Min_Temperature_Record;
 	}
 
 }
